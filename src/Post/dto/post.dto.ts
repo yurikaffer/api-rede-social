f3063e9a8fa0f75a67a1dbccc5a699babdf65d27
Yuri Kaffer <yurikaffer@gmail.com>
2024-02-20 17:32:20 -0300

    update

--- src/Post/dto/post.dto.ts
+++ src/Post/dto/post.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmpty, IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
 
 export class PostDto {
     @IsNotEmpty()
