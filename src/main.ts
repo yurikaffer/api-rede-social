13c7906b9ea6bf8d9f9a478f3fe51ebcdfba7349
Yuri Kaffer <yurikaffer@gmail.com>
2024-02-20 18:45:07 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,10 @@ import "reflect-metadata"
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cors({
-    origin: 'https://rede-social-frontend.vercel.app/', 
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  }));
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3333);
 }
