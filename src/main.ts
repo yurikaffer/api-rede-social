f44a433af29b46f498ce99306279676929fbb555
Yuri Kaffer <yurikaffer@gmail.com>
2024-02-20 16:34:26 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,16 @@ import "reflect-metadata"
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-      origin: true,
-      credentials: true,
-      allowedHeaders:
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-      maxAge: 3600,
-      exposedHeaders: 'Authorization',
-    });
-
+  app.use(cors({
+    origin: 'https://rede-social-frontend.vercel.app/', 
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
